9bf0d94f930e0c8792c18d959f5d9853be5d07ad
Lusanda Tsilana <124098032+LusandaTsilana@users.noreply.github.com>
2023-10-25 12:26:03 +0200

    navbar creation & styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Main } from "./pages/main";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </header>
+
+      <body></body>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import "../components/Navbar.css";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import logo from "../images/insta.png";
+
+export const Navbar = () => {
+  return (
+    <div id="navbar">
+      <Container>
+        <Nav className="me-auto">
+          <div>
+            <img src={logo} alt="logo" />
+          </div>
+
+          <div className="link">
+            <Link to="/" className="link">
+              Home
+            </Link>
+            <Link to="/login" className="link">
+              Login
+            </Link>
+          </div>
+        </Nav>
+      </Container>
+    </div>
+  );
+};
